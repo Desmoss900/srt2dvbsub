e951b0840ea9f20afd7b9701dde694dea2fcfd3b
Desmoss900 <Desmoss900@users.noreply.github.com>
2025-11-19 12:13:44 +0100

    Delete test/run_hd.sh

--- test/run_hd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-srt2dvbsub \
-	--input ../../dvbzoo/build/Alita_Battle_Angel.bluray.1080p_25fps_ac3_eng_aac_eng_ac3_ger_aac_ger.ts \
-	--output ../test/Alita_Battle_Angel.bluray.1080p_25fps_ac3_eng_aac_eng_ac3_ger_aac_ger_eng_dvbsub_eng.ts \
-	--srt ../test/Alita.Battle.Angel.2019.en.25.srt,../test/Alita.Battle.Angel.2019.de.25.srt \
-	--languages eng,deu \
-	--ssaa 8 
